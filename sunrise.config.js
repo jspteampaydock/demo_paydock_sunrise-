44b0f68875a31da818a8912927d775f125be428c
jspteampaydock <157017480+jspteampaydock@users.noreply.github.com>
2024-09-09 17:12:46 +0300

    Change config

--- sunrise.config.js
+++ sunrise.config.js
@@ -12,12 +12,12 @@ const config = {
   ct: {
     auth: {
       host: 'https://auth.europe-west1.gcp.commercetools.com',
-      projectKey: 'demo-paydock',
+      projectKey: 'demo2-paydock',
       credentials: {
-        clientId: 'PK9WaT4yeWzJX2f3Vs_RpAZp',
-        clientSecret: 'bjQbof1-0sAjhGHRnmh-9tU1-cAVR90r',
+        clientId: 'VAcpYpCl1Xft4VXUBDiGqpz7',
+        clientSecret: 'fEUEzWFvUA1G496YZ7kxriJyjm23gGvO',
       },
-      scope: 'manage_project:demo-paydock view_audit_log:demo-paydock manage_api_clients:demo-paydock view_api_clients:demo-paydock'
+      scope: 'manage_payments:demo2-paydock view_quotes:demo2-paydock manage_order_edits:demo2-paydock manage_import_containers:demo2-paydock manage_associate_roles:demo2-paydock manage_orders:demo2-paydock manage_customer_groups:demo2-paydock view_api_clients:demo2-paydock manage_product_selections:demo2-paydock view_audit_log:demo2-paydock view_shipping_methods:demo2-paydock manage_project:demo2-paydock manage_api_clients:demo2-paydock manage_connectors:demo2-paydock view_product_selections:demo2-paydock manage_customers:demo2-paydock manage_discount_codes:demo2-paydock manage_audit_log:demo2-paydock manage_approval_rules:demo2-paydock view_project_settings:demo2-paydock manage_cart_discounts:demo2-paydock manage_products:demo2-paydock manage_business_units:demo2-paydock view_messages:demo2-paydock view_shopping_lists:demo2-paydock manage_categories:demo2-paydock manage_checkout_payment_intents:demo2-paydock manage_approval_flows:demo2-paydock manage_attribute_groups:demo2-paydock manage_connectors_deployments:demo2-paydock view_products:demo2-paydock manage_sessions:demo2-paydock view_quote_requests:demo2-paydock manage_extensions:demo2-paydock'
     },
     api:
         'https://api.europe-west1.gcp.commercetools.com',
